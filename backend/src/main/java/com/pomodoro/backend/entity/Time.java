8270c56d45a749f9a91a4fd82f92fe6c8b7a344a
19092 <19092@esp.mr>
2021-05-18 11:13:55 +0000

    third commit

---
+++ backend/src/main/java/com/pomodoro/backend/entity/Time.java
@@ -0,0 +1,5 @@
+package com.pomodoro.backend.entity;
+
+public class Time {
+    
+}
---
+++ backend/src/test/java/com/pomodoro/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomodoro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
